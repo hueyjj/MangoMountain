be476da7874b2c08e9029497dead8e5b1f214a66
hueyjj <jasperjeng@gmail.com>
2018-05-06 17:45:16 -0700

    Require ucsc.edu email

--- api/views.py
+++ api/views.py
@@ -9,50 +9,59 @@ from .forms import SignUpForm, LoginForm
 
 @csrf_exempt
 def signup(request):
-    print("signup: Request received")
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data["email"]
+
+            email_domain = email.split('@')[1]
+            if email_domain != "ucsc.edu":
+                return HttpResponse("Require ucsc.edu email", status=400)
+
             password = form.cleaned_data["password"]
 
+            if User.objects.filter(username=email).exists():
+                return HttpResponse("User already exists", status=200)
+            
             User.objects.create_user(
                 username=email,
                 email=email,
                 password=password,
             ).save()
-            return HttpResponse("Valid form received")
-        else:
-            return HttpResponse("Invalid form received")
 
-    return HttpResponse("Request received")
+            return HttpResponse("New user created", status=200)
+        else:
+            return HttpResponse("Invalid form received", status=400)
+
+    return HttpResponse("Request received, but did not satisfy any conditions", status=200)
 
 
 @csrf_exempt
 def login_user(request):
     if request.method == "POST":
-       form = LoginForm(request.POST)
-       if form.is_valid():
-           email = form.cleaned_data["email"]
-           password = form.cleaned_data["password"]
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
 
-           user = authenticate(request, username=email, password=password)
-           if user:
-               login(request, user)
-               return HttpResponse("Login success")
+            user = authenticate(request, username=email, password=password)
 
-           return HttpResponse("No user found")
-       # TODO Log user automatically if user has valid session
-       else:
-           return HttpResponse(form.errors)
-           #return HttpResponse("Invalid form")
+            if user:
+                login(request, user)
+                return HttpResponse("Login success", status=200)
 
-    return HttpResponse("Invalid login")
+            return HttpResponse("No user found", status=400)
+        # TODO Log user automatically if user has valid session
+        else:
+            return HttpResponse(form.errors)
+            # return HttpResponse("Invalid form")
+
+    return HttpResponse("Invalid login", status=400)
 
 
 @csrf_exempt
 def logout_user(request):
     if request.method == "POST":
         logout(request)
-        return HttpResponse("Logout successful")
-    return HttpResponse("Require POST request to logout")
+        return HttpResponse("Logout successful", status=200)
+    return HttpResponse("Require POST request to logout", status=400)
