1641326f0c27827f93b7119ef9ae6a6d8d6a6885
hueyjj <jasperjeng@gmail.com>
2018-05-09 15:32:17 -0700

    Trying to get postgresql to work with heroku

--- MangoMountain/settings.py
+++ MangoMountain/settings.py
@@ -84,9 +84,17 @@ WSGI_APPLICATION = 'MangoMountain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'skadoosh',
+        'USER': 'postgres',
+        'PASSWORD': 'xxxx',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
@@ -127,6 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import django_heroku
+django_heroku.settings(locals())
 
 # Heroku Postgresql connection
 import dj_database_url
