2e251beb8bb26014e676086846fc6daa2a1b4976
hueyjj <jasperjeng@gmail.com>
2018-05-18 00:11:06 -0700

    Saving work on fixtures

--- api/models.py
+++ api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class SectionLab(models.Model):
+    """
+    Model representing a section and lab
+    """
+    sectionlab = models.CharField(max_length=999)
+
+    def __str__(self):
+        return ""
+
+
+class Course(models.Model):
+    """
+    Model representing a course.
+    """
+
+    title = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=999, blank=True)
+    class_notes = models.CharField(max_length=999, blank=True)
+
+    available_seats = models.IntegerField(default=0)
+    career = models.CharField(max_length=99, blank=True)
+    class_num = models.IntegerField(default=0, blank=True)
+    credits = models.CharField(max_length=99, blank=True)
+    enrolled = models.IntegerField(default=0)
+    enrollment_capacity = models.IntegerField(default=0)
+    general_education = models.CharField(max_length=99, blank=True)
+    grading = models.CharField(max_length=99, blank=True)
+    status = models.CharField(max_length=99, blank=True)
+    type = models.CharField(max_length=99, blank=True)
+    waitlist_capacity = models.IntegerField(default=0)
+    waitlist_total = models.IntegerField(default=0)
+
+    days_and_times = models.CharField(max_length=99, blank=True)
+    instructor = models.CharField(max_length=99, blank=True)
+    meeting_dates = models.CharField(max_length=99, blank=True)
+    room = models.CharField(max_length=99, blank=True)
+
+    section_and_labs = models.ForeignKey(SectionLab, on_delete=models.SET_NULL, null=True) 
+
+    def __str__(self):
+        return self.title
